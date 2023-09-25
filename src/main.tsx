799bf6a9797c8a46a1bc920aaa658c9adedd7c1b
pengsy <songyang410@outlook.com>
2023-09-25 15:04:07 +0800

    update 关闭严格模式,严格模式导致页面渲染两次，不利于调试

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { Provider } from 'react-redux';
@@ -11,11 +10,9 @@ import "./styles.css";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-                <App />
-            </PersistGate>
-        </Provider>
-    </React.StrictMode>
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
+    </Provider>
 );
